cb04a3bdeb2a5446b04402c91cadd097aebbc9d0
Bossanogi <blackfire502@gmail.com>
2024-03-02 02:37:44 +0300

    wip(pig_latin): implement convert, pig_vowel, and pig_consonant functions

--- src/pig_latin/func.rs
+++ src/pig_latin/func.rs
@@ -7,15 +7,33 @@ pub fn convert(text: &String) -> String {
 
     let mut pig_words: Vec<String> = Vec::new();
     let words = text.split_whitespace();
+    let consonant_reg = Regex::new(r"[[:<:][bcdfjghklmnprstvwxz]").unwrap();
+    let vowel_reg = Regex::new("r[[:<:]][aeiouy]").unwrap();
     
     for word in words {
-        let first_ch = word.chars().nth(0);
-        match first_ch {
-            
+        let mut pig_word = String::new();
+        if consonant_reg.is_match(word) {
+            pig_word = pig_consonant(&word);
+        } else if  vowel_reg.is_match(word){
+            pig_word = pig_vowel(&word);
         }
+        pig_words.push(pig_word);
     }
     
-    
-    let result = "Pig latin text";
-    result.to_string()
+    pig_words.join(" ").to_string()
+}
+
+fn pig_consonant(word: &str) -> String {
+    let mut buffer = String::new();
+    let (first, last) = word.split_at(1);
+    buffer.push_str(last);
+    buffer.push_str("ay");
+    buffer.push_str(first);
+    buffer
+}
+
+fn pig_vowel(word: &str) -> String {
+    let mut buffer = word.to_string();
+    buffer.push_str("hay");
+    buffer
 }

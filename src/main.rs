d062af43cfdf76d1226bc30dd205791e74cffbfc
Bossanogi <blackfire502@gmail.com>
2024-03-02 01:44:59 +0300

    wip(pig_latin): implement function take_string, partial implement function convert.
    convert function is now using regex to filtr first symbol of a word

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+
+use std::io::stdin;
+
 pub mod median_ex;
 pub mod pig_latin;
+
 
 fn main() {
     //First exercise: from list of integers take median and mode
@@ -13,8 +17,23 @@ fn main() {
     println!["{:?}", numbers2];
 
     //Second exercise: from string of text take pig latin string
-    let text = String::from("As expected! This is last warning!");
+    let text = take_string();
     let pig_text = pig_latin::convert(&text);
 
     println!("{text} to pig latin: {pig_text}");
+}
+
+fn take_string() -> String {
+    println!("Say your words");
+    let mut buffer = String::new();
+    let stdin = match stdin().read_line(&mut buffer) {
+        Ok(i) => i,
+        Err(e) => {println!("{e}"); 0},
+    };
+
+    if stdin != 0 {
+        buffer.trim_end().to_string()
+    } else {
+        "".to_string()
+    }
 }
--- src/pig_latin/func.rs
+++ src/pig_latin/func.rs
@@ -1,4 +1,21 @@
-pub fn convert(_text: &String) -> String {
+use regex::Regex;
+
+pub fn convert(text: &String) -> String {
+    if text.len() == 0 {
+        panic!("Nothing to convert!");
+    };
+
+    let mut pig_words: Vec<String> = Vec::new();
+    let words = text.split_whitespace();
+    
+    for word in words {
+        let first_ch = word.chars().nth(0);
+        match first_ch {
+            
+        }
+    }
+    
+    
     let result = "Pig latin text";
     result.to_string()
 }
